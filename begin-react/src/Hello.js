fcf417ecd39b6abadad87db59ae49a97c78b873d
Jangyusu <dbtnss@naver.com>
2020-04-26 15:58:08 +0900

    리액트 배우기

---
+++ begin-react/src/Hello.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Hello({ color, name, isSpecial }) {
+    return (
+        <div style={{
+            color
+        }}>
+            <b>{isSpecial ? "스페셜하다!" : "낫스페셜"}</b>
+            안녕하세요 {name}
+        </div>
+    );
+}
+
+Hello.defaultProps = {
+    name: "이름없음"
+}
+export default Hello;
