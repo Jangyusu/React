ba999c930c0472b51ba266a13e0226f4bca3bf10
Jangyusu <dbtnss@naver.com>
2020-05-09 16:31:19 +0900

    배열 추가 제거 수정

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,34 +1,80 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import UserList from './UserList';
+import CreateUser from './CreateUser';
 
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+  });
+  const { username, email } = inputs;
+  const onChange = e => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+
+  const [users, setUsers] = useState([
     {
       id: 1,
-      username: "a",
-      email: "a@gmaile.com"
+      username: 'velopert',
+      email: 'public.velopert@gmail.com',
+      active: true,
     },
     {
       id: 2,
-      username: "b",
-      email: "b@gmaile.com"
+      username: 'tester',
+      email: 'tester@example.com',
+      active: false,
     },
     {
       id: 3,
-      username: "c",
-      email: "c@gmaile.com"
+      username: 'liz',
+      email: 'liz@example.com',
+      active: false,
     }
-  ];
+  ]);
 
   const nextId = useRef(4);
 
   const onCreate = () => {
-    console.log(nextId.current); // 4
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    }
+    setUsers(users.concat(user));
+    setInputs({
+      username: '',
+      email: ''
+    });
     nextId.current += 1;
   }
 
+  const onRemove = id => {
+    setUsers(users.filter(user => user.id !== id));
+  }
+
+  const onToggle = id => {
+    setUsers(users.map(
+      user => user.id === id
+        ? { ...user, active: !user.active }
+        : user
+    ))
+  }
+
   return (
-    <UserList users={users} />
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+    </>
   );
 }
 
--- example-react/src/FontSize.js
+++ example-react/src/FontSize.js
@@ -3,12 +3,13 @@ import React, { useState } from 'react';
 function FontSize() {
     const [number, setNumber] = useState(30);
     const style = {
-        fontSize: number
+        fontSize: number,
+        width: 1000,
+        height: 100
     }
 
     const onIncrease = () => {
         setNumber(prevNumber => prevNumber + 1);
-        console.log(number);
     }
 
     const onDecrease = () => {
