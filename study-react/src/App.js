0b528fc6e60fc9d4090477c758cce6ce976deeae
Jangyusu <dbtnss@naver.com>
2020-06-13 22:18:30 +0900

    delete 구현

--- study-react/src/App.js
+++ study-react/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
     super(props);
     this.max_content_id = 3;
     this.state = {
-      mode: 'create',
+      mode: 'welcome',
       selected_content_id: 2,
       subject: { title: 'WEB', sub: 'world wide web!' },
       welcome: { title: 'Welcome', desc: 'Hello, React!!' },
@@ -117,9 +117,26 @@ class App extends Component {
         />
         <Control
           onChangeMode={function (_mode) {
-            this.setState({
-              mode: _mode,
-            });
+            if (_mode === 'delete') {
+              if (window.confirm('really?')) {
+                var _contents = Array.from(this.state.contents);
+                for (var i = 0; i < this.state.contents.length; i++) {
+                  if (_contents[i].id === this.state.selected_content_id) {
+                    _contents.splice(i, 1);
+                    break;
+                  }
+                }
+                this.setState({
+                  contents: _contents,
+                  mode: 'welcome',
+                });
+                alert('deleted!');
+              }
+            } else {
+              this.setState({
+                mode: _mode,
+              });
+            }
           }.bind(this)}
         />
         {this.getContent()}
