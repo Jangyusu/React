cb9c8de09a2b3213b7efae967e5de523cf7131a1
Jangyusu <dbtnss@naver.com>
2020-06-13 19:36:18 +0900

    contents변경

--- study-react/src/App.js
+++ study-react/src/App.js
@@ -9,6 +9,7 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
+    this.nax_content_id = 3;
     this.state = {
       mode: 'create',
       selected_content_id: 2,
@@ -45,7 +46,15 @@ class App extends Component {
       _article = (
         <CreateContent
           onSubmit={function (_title, _desc) {
-            console.log(_title, _desc);
+            this.max_content_id++;
+            var _contents = this.state.contents.concat({
+              id: this.max_content_id,
+              title: _title,
+              desc: _desc,
+            });
+            this.setState({
+              contents: _contents,
+            });
           }.bind(this)}
         />
       );
