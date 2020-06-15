95e3c968c78550f5232bda40a61b7297add1fd46
Jangyusu <dbtnss@naver.com>
2020-06-15 09:13:10 +0900

    useMemo를 사용하여 연산한 값 재사용하기

--- todolist-react/src/UserList.js
+++ todolist-react/src/UserList.js
@@ -1,11 +1,7 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 function User({ user, onRemove, onToggle }) {
   const { username, email, id, active } = user;
-
-  useEffect(() => {
-    console.log(user);
-  }, [user]);
 
   return (
     <div>
