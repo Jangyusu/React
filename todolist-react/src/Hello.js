f588aab20b8b59c934135f551e23e8ef75f3ccaa
Jangyusu <dbtnss@naver.com>
2020-06-15 18:43:02 +0900

    클래스형 컴포넌트 소개

--- todolist-react/src/Hello.js
+++ todolist-react/src/Hello.js
@@ -1,20 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Hello({ name, color, isSpecial }) {
-  return (
-    <div
-      style={{
-        color,
-      }}
-    >
-      <b>{isSpecial ? '스페셜하다!' : '낫스페셜'}</b>
-      안녕하세요 {name}
-    </div>
-  );
+class Hello extends Component {
+  static defaultProps = {
+    name: '이름없음',
+  };
+
+  render() {
+    const { color, isSpecial, name } = this.props;
+    return (
+      <div
+        style={{
+          color,
+        }}
+      >
+        <b>{isSpecial ? '스페셜하다!' : '낫스페셜'}</b>
+        안녕하세요 {name}
+      </div>
+    );
+  }
 }
 
-Hello.defaultProps = {
-  name: '이름없음',
-};
+// function Hello({ name, color, isSpecial }) {
+//   return (
+//     <div
+//       style={{
+//         color,
+//       }}
+//     >
+//       <b>{isSpecial ? '스페셜하다!' : '낫스페셜'}</b>
+//       안녕하세요 {name}
+//     </div>
+//   );
+// }
+
+// Hello.defaultProps = {
+//   name: '이름없음',
+// };
 
 export default Hello;
