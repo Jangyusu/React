56cb383c85d192ccfd4c294c36cd50e4d56c15b9
Jangyusu <dbtnss@naver.com>
2020-06-16 15:29:50 +0900

    함수형 컴포넌트를 이용한 구구단게임 만들기

--- game-react/src/App.js
+++ game-react/src/App.js
@@ -1,32 +1,52 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './App.css';
-import Random from './Random';
-import Player from './Player';
 
-function App() {
-  const [state, setState] = useState({
-    text: '...',
-    result: '...',
-  });
-  const { text, result } = state;
+const App = () => {
+  const [first, setFirst] = useState(Math.ceil(Math.random() * 9));
+  const [second, setSecond] = useState(Math.ceil(Math.random() * 9));
+  const [value, setValue] = useState('');
+  const [result, setResult] = useState(''); // Hooks state 사용법
+  const input = useRef(null); // Hooks ref 사용법
 
-  function onClick(e) {
-    const clickItem = e.target.innerText;
+  const _onChange = e => {
+    // state 변경은 각각 해줘야 합니다.
+    // 기본 Class 에서는 setState 메소드를 이용하여 여러가지를 한번에 변경 할 수도 있었지만, Hooks에서는 아닙니다.
+    setValue(e.target.value);
+  };
 
-    setState({
-      ...state,
-      text: clickItem,
-    });
+  const _onSubmit = e => {
+    e.preventDefault();
 
-    console.log();
-  }
+    if (first * second === parseInt(value)) {
+      // 기본 Class 에서는 setState 메소드를 이용하여 여러가지를 한번에 변경 할 수도 있었지만, Hooks에서는 아닙니다.
+      setResult(value + ' Correct');
+      setFirst(Math.ceil(Math.random() * 9));
+      setSecond(Math.ceil(Math.random() * 9));
+      setValue('');
+
+      input.current.focus(); // Cursor를 옮겨준다.
+    } else {
+      setResult(value + ' Wrong');
+      setValue('');
+
+      input.current.focus();
+    }
+  };
+
+  console.log('Rendering !!');
 
   return (
-    <div className="App">
-      <Random text={text} />
-      <Player onClick={onClick} result={result} />
-    </div>
+    <>
+      <div>
+        {first} * {second} = ?
+      </div>
+      <form onSubmit={_onSubmit}>
+        <input ref={input} onChange={_onChange} type="number" value={value} />
+        <button>Insert!</button>
+      </form>
+      <div>{result}</div>
+    </>
   );
-}
+};
 
 export default App;
