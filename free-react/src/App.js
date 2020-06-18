bd314f864870639da740f505de822f6e682f9131
Jangyusu <dbtnss@naver.com>
2020-06-18 10:08:43 +0900

    로그인 창 만들기

--- free-react/src/App.js
+++ free-react/src/App.js
@@ -3,16 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App btn-group btn-group-justified">
-      <button type="button" className="btn btn-warning ">
-        Warning
-      </button>
-      <button type="button" className="btn btn-danger ">
-        Danger
-      </button>
-      <button type="button" className="btn btn-link ">
-        Link
-      </button>
+    <div className="App">
+      <form className="form">
+        <input type="id" id="id" placeholder="아이디"></input>
+        <input type="password" id="pw" placeholder="비밀번호"></input>
+        <button type="button" className="btn btn-primary">
+          로그인
+        </button>
+      </form>
     </div>
   );
 }
