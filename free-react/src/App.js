840ed01e2d9e76ba2bd67eb34ca562f3c2e5e581
Jangyusu <dbtnss@naver.com>
2020-06-19 15:24:06 +0900

    dashboard 만들기

--- free-react/src/App.js
+++ free-react/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import './App.css';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 import Login from './Login';
+// import Dashboard from './Dashboard';
+import './App.css';
 
 function App() {
-  return <Login />;
+  return (
+    <Router>
+      <Route exact path="/" component={Login}></Route>
+      {/* <Route path="/Dashbord" component={Dashboard}></Route> */}
+    </Router>
+  );
 }
 
 export default App;
---
+++ free-react/src/Dashboard.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import App from './dashboard/src/App';
+
+function Dashboard() {
+  return <App />;
+}
+
+export default Dashboard;
