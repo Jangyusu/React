bb9e6288e974a653f3ec5d6bd88890549b2222d6
Jangyusu <dbtnss@naver.com>
2020-06-19 15:23:53 +0900

    대시보드 만들기

--- free-react/src/Login.js
+++ free-react/src/Login.js
@@ -1,36 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Login() {
   return (
-    <div className="Login">
+    <div className="login">
       <form className="form">
-        <div class="input-group">
-          <span class="input-group-addon">
-            <i class="glyphicon glyphicon-user"></i>
+        <div className="input-group">
+          <span className="input-group-addon">
+            <i className="glyphicon glyphicon-user"></i>
           </span>
           <input
             id="id"
             type="text"
-            class="form-control"
+            className="form-control"
             name="id"
             placeholder="ID"
           />
         </div>
-        <div class="input-group">
-          <span class="input-group-addon">
-            <i class="glyphicon glyphicon-lock"></i>
+        <div className="input-group">
+          <span className="input-group-addon">
+            <i className="glyphicon glyphicon-lock"></i>
           </span>
           <input
             id="password"
             type="password"
-            class="form-control"
+            className="form-control"
             name="password"
             placeholder="PASSWORD"
           />
         </div>
-        <button type="submit" className="btn btn-primary">
-          로그인
-        </button>
+        <Link to="/Dashbord">
+          <button type="submit" className="btn btn-primary">
+            로그인
+          </button>
+        </Link>
       </form>
     </div>
   );
